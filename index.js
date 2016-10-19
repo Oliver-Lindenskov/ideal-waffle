b46cff110688cb4f5d6dd850a1e05808dd57b8ba
Oliver lindenskov <Oliver.lindenskov157@gmail.com>
2016-10-19 14:15:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -65,5 +65,5 @@ io.on('connection', function(socket){
 //});
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, http.settings.env);
 });
